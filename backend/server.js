91a32d4e64e0a6c92bc66be0934104e230fb58dd
ghanicodes <abdulghaniag1010@gmail.com>
2026-02-26 14:14:27 +0500

    (change url)

--- backend/server.js
+++ backend/server.js
@@ -12,14 +12,26 @@ const dashboardRoutes = require("./routes/dashboardRoutes");
 const app = express();
 
 // CORS
+// app.use(
+//     cors({
+//         origin: "http://localhost:5173" || "*",
+//         credentials: true,
+//         methods: ["GET", "POST", "PUT", "DELETE"],
+//         allowedHeaders: ["Content-Type", "Authorization"],
+//     })
+// );
+
 app.use(
-    cors({
-        origin: "http://localhost:5173" || "*",
-        credentials: true,
-        methods: ["GET", "POST", "PUT", "DELETE"],
-        allowedHeaders: ["Content-Type", "Authorization"],
-    })
+  cors({
+    origin: process.env.VITE_BACKEND_URL,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
 );
+
+
+
 app.use(cookieParser());
 app.use(express.json());
 
